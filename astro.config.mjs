8250b934e6e5b7ae28b90005c36d752852a3a029
Mikkel Jørgensen <mikkel@codemonkey.computer>
2026-05-01 20:12:17 +0200

    feat: add clerk auth
    - Added initial clerk auth to project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,15 @@ import react from '@astrojs/react';
 
 import netlify from '@astrojs/netlify';
 
+import clerk from '@clerk/astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
+  integrations: [react(), clerk()],
   adapter: netlify(),
   output: 'server'
 });
