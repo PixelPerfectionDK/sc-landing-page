f778158c28d1f7bb7a80e77fd673887b43c8cbbd
Mikkel Jørgensen <mikkel@codemonkey.computer>
2026-05-02 23:32:01 +0200

    feat: add seperate signin page
    - Added a seperate signin page for clerk auth.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,12 @@
-import { clerkMiddleware } from "@clerk/astro/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-export const onRequest = clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/signin(.*)"]);
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  if (!isPublicRoute(context.request)) {
+    const authObject = auth();
+    if (!authObject.userId) {
+      return authObject.redirectToSignIn();
+    }
+  }
+});
