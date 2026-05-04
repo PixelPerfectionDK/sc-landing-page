63ca64bc4e35c2da41056f4646e8b9744b9b8357
Mikkel Jørgensen <mikkel@codemonkey.computer>
2026-05-04 20:29:02 +0200

    chore: disable astro dev bar
    - Disabled the astro development menu bar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,35 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-import clerk from '@clerk/astro';
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  fonts: [{
-    provider: fontProviders.google(),
-    name: 'Manrope',
-    cssVariable: '--font-manrope',
-  },
-  {
-    provider: fontProviders.google(),
-    name: 'JetBrains Mono',
-    cssVariable: '--font-jetbrains-mono',
-  }
-],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Manrope",
+      cssVariable: "--font-manrope",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "JetBrains Mono",
+      cssVariable: "--font-jetbrains-mono",
+    },
+  ],
   integrations: [react(), clerk()],
   adapter: netlify(),
-  output: 'server'
+  output: "server",
 });
